e258342ec807b2aadf0213f2fdbd5e76ab05fcec
Jose Gordillo Mendoza <jgordillome@unsa.edu.pe>
2023-07-28 19:27:40 -0500

    primer borrador de pagina web, sujeto a pruebas

--- libreria_project/almacen/views.py
+++ libreria_project/almacen/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import product
 
 # Create your views here.
+def ObjetoTienda(request):
+    obj = product.objects.all()
+    categorias = []
+    for producto in obj:
+        if producto.categoria not in categorias :
+            categorias.append(producto.categoria)
+    context ={
+      'productos': obj,
+      'categorias': categorias,
+    }
+    return render(request, 'almacen/index.html', context)
+
+def obtener_productos(request):
+    categoria = request.GET.get('categoria')
+    if categoria == 'Todos':
+        productos = product.objects.all()
+    else:
+        productos = product.objects.filter(categoria=categoria)
+    context = {
+        'productos': productos
+    }
+    return render(request, 'almacen/productos.html', context)
--- libreria_project/libreria_project/urls.py
+++ libreria_project/libreria_project/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
+from almacen.views import ObjetoTienda, obtener_productos
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='almacen/', permanent=True), name="otro"),
     path('admin/', admin.site.urls),
+    path('almacen/',ObjetoTienda,name='otro'),
+    path('obtener_productos/',obtener_productos, name='productos'),
 ]
