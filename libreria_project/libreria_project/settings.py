b4ff3ed353ccc28896adb7863ec26c9516d350a9
Jose Gordillo Mendoza <jgordillome@unsa.edu.pe>
2023-07-28 20:02:11 -0500

    Correo implementado

--- libreria_project/libreria_project/settings.py
+++ libreria_project/libreria_project/settings.py
@@ -122,7 +122,14 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com' # Servidor de gmail
+EMAIL_PORT = 587
+
+EMAIL_HOST_USER ='databios2023@gmail.com'
+EMAIL_HOST_PASSWORD = 'gtnjvkvnwjngjwhl' # Contraseña de aplicacion
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
